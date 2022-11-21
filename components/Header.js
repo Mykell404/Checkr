84343a25c20adefd714fe665803fb5d1707db78e
Mykell404 <milebamicheal@gmail.com>
2022-11-21 17:08:44 +0100

    added functionality to connect button

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,38 @@ import { useEffect, useState } from "react";
 
 export default function Header() {
   const [isConnected, setIsConnected] = useState(false);
+  const [hasMetamask, setHasMetamask] = useState(false);
+  const [signer, setSigner] = useState(undefined);
+  const [address, setAddress] = useState("Connect Wallet");
 
-  function connectWallet() {
-    alert("connecting");
+  useEffect(() => {
+    if (typeof window.ethereum !== "undefined") {
+      setHasMetamask(true);
+    }
+  });
+
+  async function connectWallet() {
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        await ethereum
+          .request({ method: "eth_requestAccounts" })
+          .then((result) => {
+            let walletAddress = result[0];
+            let first = walletAddress.slice(0, 5);
+            let last = walletAddress.slice(38, 42);
+            let res = `${first}...${last}`;
+            console.log(walletAddress);
+            setAddress(res);
+          });
+        setIsConnected(true);
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        setSigner(provider.getSigner());
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      setIsConnected(false);
+    }
   }
   return (
     <nav className="bg-sky-400 flex flex-wrap justify-between ">
@@ -15,7 +44,7 @@ export default function Header() {
         onClick={connectWallet}
         className="border border-slate-300 hover:border-slate-400 bg-blue-500 hover:bg-blue-600 my-4 p-2 rounded-sm"
       >
-        Connect Wallet
+        {address}
       </button>
     </nav>
   );
